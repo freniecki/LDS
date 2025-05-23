20b34b46de81c002b41e8c499b1ba1144a43d461
freniecki <247773@edu.p.lodz.pl>
2025-05-23 14:11:35 +0200

    adjustments and corrections to easily create chart for attribute and ranges

--- src/main/java/pl/frot/plot/SimplePlot.java
+++ src/main/java/pl/frot/plot/SimplePlot.java
@@ -1,27 +1,37 @@
 package pl.frot.plot;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.LogAxis;
+import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYAreaRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import pl.frot.model.AttrRanges;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class SimplePlot extends JPanel {
     private final XYSeriesCollection dataset = new XYSeriesCollection();
     private final JFreeChart chart;
-    private static String dirPath = "/home/firaanki/IOAD/ksr/projekt-2/pngs";
+    private final String dirPath;
+    private static final Random random = new Random();
 
-    public SimplePlot(String title, double domainMin, double domainMax) {
+    public SimplePlot(String title, String dirPath) {
+        this.dirPath = dirPath;
+
         chart = ChartFactory.createXYLineChart(
                 title,
                 "Wiek",
@@ -32,7 +42,9 @@ public class SimplePlot extends JPanel {
         );
         XYPlot plot = chart.getXYPlot();
         plot.getRangeAxis().setRange(0, 1.0);
-        plot.getDomainAxis().setRange(domainMin, domainMax);
+
+        plot.getDomainAxis().setAutoRange(true);
+        plot.getRangeAxis().setAutoRange(true);
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, false);
         plot.setRenderer(renderer);
@@ -43,23 +55,21 @@ public class SimplePlot extends JPanel {
         add(new ChartPanel(chart), BorderLayout.CENTER);
     }
 
-    public void addTriangularFunction(String name, double a, double b, double c, Color color) {
+    public XYSeries createTriangularSeries(String name, double a, double b, double c) {
         XYSeries series = new XYSeries(name);
         series.add(a, 0.0);
         series.add(b, 1.0);
         series.add(c, 0.0);
-        dataset.addSeries(series);
-        setColor(dataset.getSeriesCount() - 1, color);
+        return series;
     }
 
-    public void addTrapezoidalFunction(String name, double a, double b, double c, double d, Color color) {
+    public XYSeries createTrapezoidalSeries(String name, double a, double b, double c, double d) {
         XYSeries series = new XYSeries(name);
         series.add(a, 0.0);
         series.add(b, 1.0);
         series.add(c, 1.0);
         series.add(d, 0.0);
-        dataset.addSeries(series);
-        setColor(dataset.getSeriesCount() - 1, color);
+        return series;
     }
 
     private void setColor(int seriesIndex, Color color) {
@@ -69,41 +79,94 @@ public class SimplePlot extends JPanel {
         renderer.setSeriesStroke(seriesIndex, new BasicStroke(3.0f));
     }
 
-    public static void main(String[] args) {
-        plot("Rok budowy");
+    private static Color getPrettyPastelColor(){
+        float hue = random.nextFloat();
+        // Saturation between 0.1 and 0.3 -> changed to 0.5-0.7, too bright
+        float saturation = (random.nextInt(2000) + 5000) / 10000f;
+        float luminance = 0.9f;
+        return Color.getHSBColor(hue, saturation, luminance);
     }
 
-    private static void plot(String title) {
+    private static void plot(String title, String dirPath, AttrRanges ranges) {
         SwingUtilities.invokeLater(() -> {
-            SimplePlot plot = new SimplePlot("Zmienna lingwistyczna: %s".formatted(title), 1850, 2020);
+            SimplePlot plot = new SimplePlot("Zmienna lingwistyczna: %s".formatted(title), dirPath);
 
-            plot.addTrapezoidalFunction("Przedwojenny", 1850, 1850, 1914, 1939, Color.BLUE);
-            plot.addTrapezoidalFunction("Powojenny", 1918, 1945, 1960, 1970, Color.GREEN);
-            plot.addTrapezoidalFunction("Współczesny", 1950, 1980, 2020, 2020, Color.RED);
+            if (ranges.name().equals("lot")) {
+                LogAxis xAxis = new LogAxis("Powierzchnia działki (skala log.)");
+                xAxis.setBase(10);
+                xAxis.setTickUnit(new NumberTickUnit(0.5));
+                plot.chart.getXYPlot().setDomainAxis(xAxis);
 
-            XYAreaRenderer renderer = new XYAreaRenderer();
-            renderer.setSeriesPaint(0, new Color(0, 0, 255, 100));     // Młody
-            renderer.setSeriesPaint(1, new Color(0, 200, 0, 100));     // Dojrzały
-            renderer.setSeriesPaint(2, new Color(200, 0, 0, 100));     // Stary
-            plot.chart.getXYPlot().setRenderer(renderer);
+                for (Map.Entry<String, List<Double>> variable : ranges.ranges().entrySet()) {
+                    List<Double> scaledValues = variable.getValue().stream().map(v -> v / 1000).toList();
+                    ranges.ranges().put(variable.getKey(), scaledValues);
+                }
+            }
 
-            JFrame frame = new JFrame("Zmienna lingwistyczna - wykres");
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
-            frame.setContentPane(plot);
-            frame.setVisible(true);
+            if (ranges.name().equals("totalInteriorLivableArea")) {
+                LogAxis xAxis = new LogAxis("Powierzchnia użytkowa (skala log.)");
+                xAxis.setBase(10);
+                xAxis.setTickUnit(new NumberTickUnit(0.1));
+                plot.chart.getXYPlot().setDomainAxis(xAxis);
+            }
 
+            for (Map.Entry<String, List<Double>> variable : ranges.ranges().entrySet()) {
+                String name = variable.getKey();
+                double a = variable.getValue().getFirst();
+                double b = variable.getValue().get(1);
+                double c = variable.getValue().get(2);
+
+                XYSeries series = switch (variable.getValue().size()) {
+                    case 3:
+                         yield plot.createTriangularSeries(name, a, b, c);
+                    case 4:
+                        double d = variable.getValue().get(3);
+                        yield plot.createTrapezoidalSeries(name, a, b, c, d);
+                    default:
+                        throw new RuntimeException("dupa twojej starej");
+                };
+
+                plot.dataset.addSeries(series);
+                plot.setColor(plot.dataset.getSeriesCount() - 1, getPrettyPastelColor());
+            }
+
+            showPlot(title, plot);
             saveToFile(title, plot);
         });
     }
 
+    private static void showPlot(String title, SimplePlot plot) {
+        JFrame frame = new JFrame("Zmienna lingwistyczna - %s".formatted(title));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setContentPane(plot);
+        frame.setVisible(true);
+    }
+
     private static void saveToFile(String title, SimplePlot plot) {
         try {
-            ChartUtils.saveChartAsPNG(new File(dirPath + "/%s_zmienne.png".formatted(title)), plot.chart, 800, 600);
+            ChartUtils.saveChartAsPNG(
+                    Path.of(plot.dirPath + "/%s_zmienne.png".formatted(title)).toFile(),
+                    plot.chart, 600, 400);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private static List<AttrRanges> getAttrRanges(String jsonPath) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(
+                Path.of(jsonPath).toFile(),
+                new TypeReference<>(){});
+    }
 
+    public static void main(String[] args) throws IOException {
+        List<AttrRanges> ranges = getAttrRanges("src/main/resources/ranges.json");
+
+        plot(
+                "Powierzchnia_użytkowa",
+                "src/main/resources/plot",
+                ranges.get(2)
+        );
+    }
 }
