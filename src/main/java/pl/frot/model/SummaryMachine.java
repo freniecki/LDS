ec7ba823e409649af1e88880ada18f5acd0694c4
freniecki <247773@edu.p.lodz.pl>
2025-06-07 09:50:27 +0200

    base functionalities of summarizer

--- src/main/java/pl/frot/model/SummaryMachine.java
+++ src/main/java/pl/frot/model/SummaryMachine.java
@@ -1,5 +1,6 @@
 package pl.frot.model;
 
+import lombok.Getter;
 import pl.frot.data.Property;
 import pl.frot.data.TermDao;
 import pl.frot.data.DataLoader;
@@ -19,13 +20,10 @@ public class SummaryMachine {
     private static final Logger logger = Logger.getLogger(SummaryMachine.class.getName());
 
     List<Property> properties = new ArrayList<>();
+    @Getter
     List<LinguisticVariable> linguisticVariables = new ArrayList<>();
-    List<List<Label>> labelCombinations = new ArrayList<>();
+    @Getter
     List<Quantifier> quantifiers = new ArrayList<>();
-
-    List<SingleSubjectSummary> summaries = new ArrayList<>();
-    List<MultisubjectSummary> multisubjectSummaries = new ArrayList<>();
-
 
     public static void main(String[] args) {
         SummaryMachine sm = new SummaryMachine();
@@ -35,12 +33,7 @@ public class SummaryMachine {
     public void run() {
         if (!loadData()) {
             logger.warning("Failed to load data");
-            return;
         }
-        logger.info("Loaded data successfully");
-
-        createFirstTypeSingleSubjectSummaries();
-        logger.info(String.format("Created %s I type single subject summaries.", summaries.size()));
     }
 
     // ==== DATA LOADING ====
@@ -48,6 +41,7 @@ public class SummaryMachine {
     public boolean loadData() {
         try {
             properties = DataLoader.loadProperties("src/main/resources/property.csv");
+            logger.info(String.format("Loaded %s properties", properties.size()));
         } catch (FileNotFoundException e) {
             logger.warning("File 'property.csv' not found: " + e.getMessage());
             return false;
@@ -56,7 +50,7 @@ public class SummaryMachine {
         List<TermDao> linguisticVariablesDao;
         try {
             linguisticVariablesDao = DataLoader.loadTerms("src/main/resources/summarizers.json");
-
+            logger.info(String.format("Loaded %s linguistic variables", linguisticVariablesDao.size()));
         } catch (IOException e) {
             logger.warning("File 'summarizers.json' not found: " + e.getMessage());
             return false;
@@ -66,36 +60,33 @@ public class SummaryMachine {
         List<TermDao> quantifiersDao;
         try {
             quantifiersDao = DataLoader.loadTerms("src/main/resources/quantifiers.json");
+            logger.info(String.format("Loaded %s quantifiers", quantifiersDao.size()));
         } catch (IOException e) {
             logger.warning("File 'quantifiers.json' not found: " + e.getMessage());
             return false;
         }
         loadQuantifiers(quantifiersDao);
 
+        logger.info("Data loaded successfully");
         return true;
     }
 
     private void loadLinguisticVariables(List<TermDao> linguisticVariablesDao) {
         for (TermDao termDao : linguisticVariablesDao) {
             String name = termDao.name();
-            List<Label> labels = new ArrayList<>();
+            List<Double> uod = termDao.uod();
+            uod.add(1.0); // add step for continuous
             Map<String, List<Double>> ranges = termDao.ranges();
 
+            List<Label> labels = new ArrayList<>();
             for (Map.Entry<String, List<Double>> entry : ranges.entrySet()) {
                 String labelValue = entry.getKey();
-                FuzzySet<Double> fuzzySet = getDoubleFuzzySet(entry);
+                FuzzySet<Double> fuzzySet = getDoubleFuzzySet(entry.getValue(), uod);
 
                 labels.add(new Label(labelValue, fuzzySet));
             }
             linguisticVariables.add(new LinguisticVariable(name, labels));
         }
-
-        List<Label> allLabels = new ArrayList<>();
-        for (LinguisticVariable linguisticVariable : linguisticVariables) {
-            allLabels.addAll(linguisticVariable.labels());
-        }
-
-        labelCombinations = SetOperations.getCombinations(allLabels, 4);
     }
 
     private void loadQuantifiers(List<TermDao> quantifiersDao) {
@@ -106,44 +97,52 @@ public class SummaryMachine {
                 default -> throw new IllegalStateException("Unexpected value: " + quantifierDao.name());
             };
 
+            List<Double> uod = quantifierDao.uod();
+            uod.add(1.0); // add step for continuous
             Map<String, List<Double>> quantifiersLabels = quantifierDao.ranges();
 
             for (Map.Entry<String, List<Double>> entry : quantifiersLabels.entrySet()) {
                 String labelValue = entry.getKey();
-                FuzzySet<Double> fuzzySet = getDoubleFuzzySet(entry);
+                FuzzySet<Double> fuzzySet = getDoubleFuzzySet(entry.getValue(), uod);
 
                 quantifiers.add(new Quantifier(labelValue, type, fuzzySet));
             }
         }
     }
 
-    private FuzzySet<Double> getDoubleFuzzySet(Map.Entry<String, List<Double>> entry) {
-        List<Double> values = entry.getValue();
-
-        MembershipFunction<Double> membershipFunction = switch (values.size()) {
-            case 3 -> new TriangularFunction(values);
-            case 4 -> new TrapezoidalFunction(values);
-            default -> throw new IllegalStateException("Unexpected value: " + values.size());
+    private FuzzySet<Double> getDoubleFuzzySet(List<Double> funcParams, List<Double> uod) {
+        MembershipFunction<Double> membershipFunction = switch (funcParams.size()) {
+            case 3 -> new TriangularFunction(funcParams);
+            case 4 -> new TrapezoidalFunction(funcParams);
+            default -> throw new IllegalStateException("Unexpected value: " + funcParams.size());
         };
 
         return new FuzzySet<>(
-                new DiscreteUniverse(values.get(0), values.get(1), values.get(2)),
+                new ContinousUniverse(uod.getFirst(), uod.get(1), uod.get(2)),
                 membershipFunction
         );
     }
 
     // ==== SUMMARIZING ====
 
-    public void createFirstTypeSingleSubjectSummaries() {
-        for (Quantifier quantifier : quantifiers) {
+    public List<SingleSubjectSummary> createFirstTypeSingleSubjectSummaries(List<Quantifier> chosenQuantifiers, List<Label> chosenLabels) {
+        List<SingleSubjectSummary> summaries = new ArrayList<>();
+
+        List<List<Label>> labelCombinations = createLabelsCombinations(chosenLabels);
+
+        for (Quantifier quantifier : chosenQuantifiers) {
             for (List<Label> labelCombination : labelCombinations) {
                 summaries.add(new SingleSubjectSummary(
                         quantifier,
                         null,
-                        labelCombination,
-                        null
+                        labelCombination
                 ));
             }
         }
+        return summaries;
+    }
+
+    private List<List<Label>> createLabelsCombinations(List<Label> chosenLabels) {
+        return SetOperations.getCombinations(chosenLabels, 4);
     }
 }
