f6811a355a9f2cae03aa9728890bc3480dfd79ae
freniecki <247773@edu.p.lodz.pl>
2025-06-07 09:47:53 +0200

    adding uod for fuzzy set

--- src/main/java/pl/frot/data/TermDao.java
+++ src/main/java/pl/frot/data/TermDao.java
@@ -3,6 +3,6 @@ package pl.frot.data;
 import java.util.List;
 import java.util.Map;
 
-public record TermDao(String name, Map<String, List<Double>> ranges) {
+public record TermDao(String name, List<Double> uod, Map<String, List<Double>> ranges) {
 
 }
