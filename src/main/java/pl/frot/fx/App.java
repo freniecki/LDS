30795456266ff0cdb94729f937946112ccd1a6e6
freniecki <247773@edu.p.lodz.pl>
2025-06-07 09:49:54 +0200

    model injection

--- src/main/java/pl/frot/fx/App.java
+++ src/main/java/pl/frot/fx/App.java
@@ -4,14 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.frot.model.SummaryMachine;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        SummaryMachine summaryMachine = new SummaryMachine();
+        summaryMachine.run();
+
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
+
+        fxmlLoader.setControllerFactory(controllerClass -> {
+            if (controllerClass == MainController.class) {
+                MainController controller = new MainController();
+                controller.setSummaryMachine(summaryMachine);
+                return controller;
+            } else {
+                try {
+                    return controllerClass.getDeclaredConstructor().newInstance();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("CPS: signal visualizer");
+        stage.setTitle("KSR: lingwistyczne podsumowania baz danych");
         stage.setScene(scene);
         stage.show();
     }
