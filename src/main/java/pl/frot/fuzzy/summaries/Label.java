2a2411184ac23adcf2ec25274b59da4360cf3f1b
freniecki <247773@edu.p.lodz.pl>
2025-06-07 17:23:25 +0200

    access methods

--- src/main/java/pl/frot/fuzzy/summaries/Label.java
+++ src/main/java/pl/frot/fuzzy/summaries/Label.java
@@ -1,10 +1,12 @@
 package pl.frot.fuzzy.summaries;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import pl.frot.fuzzy.base.FuzzySet;
 
 @AllArgsConstructor
 public class Label {
+    @Getter
     String name;
     FuzzySet<Double> fuzzySet;
 
--- src/main/java/pl/frot/fuzzy/summaries/Quantifier.java
+++ src/main/java/pl/frot/fuzzy/summaries/Quantifier.java
@@ -3,5 +3,8 @@ package pl.frot.fuzzy.summaries;
 import pl.frot.fuzzy.base.FuzzySet;
 
 public record Quantifier(String name, QuantifierType type, FuzzySet<Double> fuzzySet) {
-
+    @Override
+    public String toString() {
+        return name;
+    }
 }
