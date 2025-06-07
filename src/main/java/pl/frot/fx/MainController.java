5c00197aa5274e117e473adaed59d2682ffdf685
freniecki <247773@edu.p.lodz.pl>
2025-06-07 09:49:47 +0200

    model injection and container+controller setup

--- src/main/java/pl/frot/fx/MainController.java
+++ src/main/java/pl/frot/fx/MainController.java
@@ -2,35 +2,43 @@ package pl.frot.fx;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import lombok.Getter;
+import lombok.Setter;
 import pl.frot.data.SummaryDto;
+import pl.frot.model.SummaryMachine;
 
 import java.io.IOException;
 
 public class MainController {
 
+    @Getter @Setter
+    private SummaryMachine summaryMachine;
+
     @FXML private HBox topContainer;
     @FXML private VBox parametersContainer;
-    @FXML private TableView<SummaryDto> summaryContainer;
+    @FXML private ScrollPane summaryContainer;
 
     @FXML
     public void initialize() throws IOException {
         FXMLLoader topLoader = new FXMLLoader(getClass().getResource("top.fxml"));
         HBox topHbox = topLoader.load();
-        TopController topController = topLoader.getController();
         topContainer.getChildren().add(topHbox);
+        //TopController topController = topLoader.getController();
 
         FXMLLoader parametersLoader = new FXMLLoader(getClass().getResource("parameters.fxml"));
         VBox parametersVbox = parametersLoader.load();
-        ParametersController parametersController = parametersLoader.getController();
         parametersContainer.getChildren().add(parametersVbox);
+        ParametersController parametersController = parametersLoader.getController();
+        parametersController.setSummaryMachine(summaryMachine);
 
         FXMLLoader summaryLoader = new FXMLLoader(getClass().getResource("summaries.fxml"));
-        VBox summaryVbox = summaryLoader.load();
-        SummaryController summaryController = summaryLoader.getController();
-        //summaryContainer.getChildren().add(summaryVbox);
+        ScrollPane summaryVbox = summaryLoader.load();
+        summaryContainer.setContent(summaryVbox);
+        //SummaryController summaryController = summaryLoader.getController();
     }
 
 
