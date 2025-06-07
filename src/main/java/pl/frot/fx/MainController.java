72d473f4a1ec1a15f9bbdde40a5d0e45ee26be5b
freniecki <247773@edu.p.lodz.pl>
2025-06-07 17:25:46 +0200

    proper loading and injections; MainController as central point for control flow

--- src/main/java/pl/frot/fx/MainController.java
+++ src/main/java/pl/frot/fx/MainController.java
@@ -3,17 +3,20 @@ package pl.frot.fx;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
 import lombok.Setter;
-import pl.frot.data.SummaryDto;
 import pl.frot.model.SummaryMachine;
 
 import java.io.IOException;
 
 public class MainController {
+
+    @Getter
+    private ParametersController parametersController;
+    private TopController topController;
+    private SummaryController summaryController;
 
     @Getter @Setter
     private SummaryMachine summaryMachine;
@@ -24,22 +27,34 @@ public class MainController {
 
     @FXML
     public void initialize() throws IOException {
-        FXMLLoader topLoader = new FXMLLoader(getClass().getResource("top.fxml"));
-        HBox topHbox = topLoader.load();
-        topContainer.getChildren().add(topHbox);
-        //TopController topController = topLoader.getController();
-
+        // load views, put to containers, create Controllers for dependencies
         FXMLLoader parametersLoader = new FXMLLoader(getClass().getResource("parameters.fxml"));
         VBox parametersVbox = parametersLoader.load();
         parametersContainer.getChildren().add(parametersVbox);
-        ParametersController parametersController = parametersLoader.getController();
-        parametersController.setSummaryMachine(summaryMachine);
+        parametersController = parametersLoader.getController();
+
+        FXMLLoader topLoader = new FXMLLoader(getClass().getResource("top.fxml"));
+        HBox topHbox = topLoader.load();
+        topContainer.getChildren().add(topHbox);
+        topController = topLoader.getController();
 
         FXMLLoader summaryLoader = new FXMLLoader(getClass().getResource("summaries.fxml"));
         ScrollPane summaryVbox = summaryLoader.load();
         summaryContainer.setContent(summaryVbox);
-        //SummaryController summaryController = summaryLoader.getController();
+        summaryController = summaryLoader.getController();
+
+        // add dependencies between controllers
+        parametersController.setMainController(this);
+        topController.setMainController(this);
+        summaryController.setMainController(this);
+
+        // load model to controllers
+        parametersController.setSummaryMachine(summaryMachine);
+        summaryController.setSummaryMachine(summaryMachine);
     }
 
+    public void createSingleSubjectSummaries() {
+        summaryController.createSingleSubjectSummaries();
+    }
 
 }
