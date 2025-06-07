3ed332bc4371476587ad77d71bfc81b57b0c152c
freniecki <247773@edu.p.lodz.pl>
2025-06-07 09:50:06 +0200

    treeview of summarizers

--- src/main/java/pl/frot/fx/SummaryController.java
+++ src/main/java/pl/frot/fx/SummaryController.java
@@ -1,4 +1,10 @@
 package pl.frot.fx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import pl.frot.data.SummaryDto;
+
 public class SummaryController {
+    @FXML private TableView<SummaryDto> summaryTable;
+
 }
