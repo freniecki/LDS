f859532be208a29b749fb9b9892827ca3cec7f4a
freniecki <247773@edu.p.lodz.pl>
2025-06-12 23:37:35 +0200

    just send fuzzy set

--- src/main/java/pl/frot/model/NewLabelDto.java
+++ src/main/java/pl/frot/model/NewLabelDto.java
@@ -1,7 +1,6 @@
 package pl.frot.model;
 
-import pl.frot.fuzzy.base.MembershipFunction;
-import pl.frot.fuzzy.base.Universe;
+import pl.frot.fuzzy.base.FuzzySet;
 
-public record NewLabelDto(LabelType labelType, String name, Universe<Double> universe, MembershipFunction<Double> membershipFunction) {
+public record NewLabelDto(LabelType labelType, String name, FuzzySet<Double> fuzzySet) {
 }
